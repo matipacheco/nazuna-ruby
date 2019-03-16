9eacdba525a03ffc7bca55eab209292c37e24e1d
Mati <matipacheco@gmail.com>
2019-03-16 16:24:22 -0300

    using JRuby, Sinatra and Sidekiq together for the first time. It worked on the first try :smirk:

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
+require 'json'
 require 'sinatra'
+require_relative 'lib/nazuna'
 
 get '/' do
-  'Holi!'
+  begin
+    Nazuna.perform_async()
+
+  rescue Exception => exception
+    return { status: 500, message: exception.message }.to_json
+
+  ensure
+    return { status: 200, message: 'Job done!' }.to_json
+  end
 end
 
--- lib/nazuna.rb
+++ lib/nazuna.rb
@@ -1,3 +1,14 @@
-class Nazuna
+require 'sidekiq'
 
+# require 'java'
+# require 'path/to/jar/file'
+
+# run with bundle exec sidekiq -r ./lib/nazuna.rb on the nazuna_ruby directory
+class Nazuna
+  include Sidekiq::Worker
+
+  def perform
+    puts "Holi!"
+    # Java::Nazuna.spy()
+  end
 end
