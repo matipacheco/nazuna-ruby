71e4ad690c50afe6580abfed8c34240db8d0c7d7
Mati <matipacheco@gmail.com>
2019-03-16 16:26:23 -0300

    documentation update

--- lib/nazuna.rb
+++ lib/nazuna.rb
@@ -3,7 +3,11 @@ require 'sidekiq'
 # require 'java'
 # require 'path/to/jar/file'
 
-# run with bundle exec sidekiq -r ./lib/nazuna.rb on the nazuna_ruby directory
+# Redis should be running, so....
+#     redis-server
+# On the nazuna_ruby directory run:
+#     bundle exec sidekiq -r ./lib/nazuna.rb
+
 class Nazuna
   include Sidekiq::Worker
 
