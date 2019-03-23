d4c5ee4ff7d55af4c329b583929004cda3b71807
Mati <matipacheco@gmail.com>
2019-03-23 17:48:02 -0300

    after a lot of time, I realized that there were templates for the WS messages :weary:... anyway, now it works

--- lib/nazuna.rb
+++ lib/nazuna.rb
@@ -1,22 +1,19 @@
-require 'pry'
 require 'date'
 require 'twilio-ruby'
 
+# Class that sends Whatsapp messages using Twilio.
 class Nazuna
-  def initialize
-    @account_sid = ENV['TWILLIO_ACCOUNT_SID']
-    @auth_token  = ENV['TWILLIO_AUTH_TOKEN']
-    @client      = Twilio::REST::Client.new(@account_sid, @auth_token)
-  end
-
-  def notify
+  def self.notify
     begin
-      binding.pry
-      m=@client.messages.create(
-        body: "The bird is in the nest! \n *" + Time.now.strftime('%H:%m') + "*",
+      account_sid = ENV['TWILLIO_ACCOUNT_SID']
+      auth_token  = ENV['TWILLIO_AUTH_TOKEN']
+      client      = Twilio::REST::Client.new(account_sid, auth_token)
+
+      client.messages.create(
+        body: 'Your ' + '🦅' + ' code is ' + Time.now.strftime('%H:%M'),
         from: 'whatsapp:' + ENV['TWILIO_PHONE_NUMBER'],
         to:   'whatsapp:' + ENV['MY_PHONE_NUMBER']
-      )
+      ).sid
 
     rescue
       raise 'Twilio was unable to send the message... stupid Twilio'
--- lib/nazuna_async.rb
+++ lib/nazuna_async.rb
@@ -1,10 +1,12 @@
 require 'sidekiq'
 require_relative 'nazuna'
 
+# Class that asynchronously uses Nazuna.
+# Made this way in case Twilio services are down.
 class NazunaAsync
   include Sidekiq::Worker
 
   def perform
-    Nazuna.new.notify
+    Nazuna.notify
   end
 end
